8561d1e783d998912286e1d54360959860bfd2c1
yuizho <yuizho3@gmail.com>
2019-02-10 18:13:24 +0900

    modify fibonacci_number
    - apply memonise

--- fibonacci_number.py
+++ fibonacci_number.py
@@ -1,10 +1,14 @@
 n = int(input())
-
+cache = [-1]*(n+1)
 
 def fibo(n):
     if n == 0 or n == 1:
-        return 1
-    return fibo(n-1) + fibo(n-2)
+        cache[n] = 1
+        return cache[n]
+    if cache[n] != -1:
+        return cache[n]
+    cache[n] = fibo(n-1) + fibo(n-2)
+    return cache[n]
 
 
 print(fibo(n))
